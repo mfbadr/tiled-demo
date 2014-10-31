a089208dc6f9ae6ee9ddc083a323590c1b58548f
Mikey Badr <mfbadr@gmail.com>
2014-10-31 10:57:37 -0500

    coins

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(300, 320, Phaser.CANVAS, 'tiled', {preload:preload, create:create, update:update});
+var game = new Phaser.Game(500, 320, Phaser.CANVAS, 'tiled', {preload:preload, create:create, update:update});
 
 function preload(){
   game.load.tilemap('map', '/assets/tiled.json', null, Phaser.Tilemap.TILED_JSON);
@@ -8,7 +8,7 @@ function preload(){
   game.load.spritesheet('balls', '/assets/balls.png', 17, 17);
 }
 
-var map, background, scenery, plants, dude, cursors;
+var map, background, scenery, money, plants, dude, cursors, ground;
 
 function create(){
   game.physics.startSystem(Phaser.Physics.ARCADE);
@@ -22,26 +22,40 @@ function create(){
   scenery.resizeWorld();
   plants  = map.createLayer('plants');
   plants.resizeWorld();
+  ground  = map.createLayer('Ground');
+
+
+  //***************COIN************//
+  money = game.add.group();
+  money.enableBody = true;
+  money.physicsBodyType = Phaser.Physics.ARCADE;
+  map.createFromObjects('Money', 49, 'coin', 0, true, false, money);
+  money.callAll('animations.add', 'animations', 'spin', [0 , 1, 2, 3, 4, 5,], 10, true);
+  money.callAll('animations.play', 'animations', 'spin');
 
   //***************DUDE************//
   dude = game.add.sprite(20, 20, 'dude');
   game.physics.arcade.enable(dude);
   dude.body.collideWorldBounds = true;
-  game.camera.follow(dude);
   dude.animations.add('left', [0, 1, 2, 3], 10, true);
   dude.animations.add('right', [5, 6, 7, 8], 10, true);
+  dude.body.gravity.y = 300;
+
+  map.setCollision(34, true, 'Ground');
 
   cursors = game.input.keyboard.createCursorKeys();
-
+  game.camera.follow(dude);
 }
 
 function update(){
+  game.physics.arcade.collide(dude, ground);
+  game.physics.arcade.collide(money, ground);
   movePlayer();
 }
 
 function movePlayer(){
   dude.body.velocity.x = 0;
-  dude.body.velocity.y = 0;
+  //dude.body.velocity.y = 0;
 
 
   if(cursors.left.isDown){
